08bdb5caeaff75c058bbd531f39e2cbb8238a962
rvk27 <kale.rv31@gmail.com>
2024-09-04 18:02:00 +0530

    Create RetrieveDatabricksPublicIP.ps1
    asdf

---
+++ RetrieveDatabricksPublicIP.ps1
@@ -0,0 +1,13 @@
+# Authenticate to Azure
+Connect-AzAccount
+
+# Specify the resource group and Databricks workspace name
+$resourceGroup = "NATGATEWAY"
+$databricksWorkspaceName = "azuredatabricks01"
+
+# Get the Databricks workspace
+$workspace = Get-AzResource -ResourceGroupName $resourceGroup -ResourceType Microsoft.Databricks/workspaces -Name $databricksWorkspaceName
+
+# Retrieve the public IP address
+$publicIp = $workspace.Properties.managedResourcePublicIp
+Write-Output "Public IP Address of Azure Databricks cluster: $publicIp"
